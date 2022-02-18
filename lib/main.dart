6b972ef8b4bd54d6bbba589c931a2ceeae4a97ae
lhsg <littlehackers.sg@gmail.com>
2022-02-18 14:15:33 +0800

    upload

--- lib/main.dart
+++ lib/main.dart
@@ -35,11 +35,7 @@ class MyApp extends StatelessWidget {
               ], // children
             ),
           ),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('31'),
+          const FavoridWidget(),
         ],
       ),
     );
@@ -108,4 +104,54 @@ class MyApp extends StatelessWidget {
         ],
       );
     } // Column _buildButtonColumn end
+}
+
+class FavoridWidget extends StatefulWidget {
+  const FavoridWidget({Key? key}) : super(key: key);
+
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoridWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 31;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: const EdgeInsets.all(0),
+          child: IconButton(
+            padding: const EdgeInsets.all(0),
+            alignment: Alignment.centerRight,
+            icon: (_isFavorited
+              ? const Icon(Icons.star)
+              : const Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: SizedBox(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }   
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+   }
 }
